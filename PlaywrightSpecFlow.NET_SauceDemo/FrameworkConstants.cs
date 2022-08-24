dbcb7dda89ceb9ebf59df47778687403624ee5c8
quality100 <quality100@yandex.ru>
2022-08-24 21:02:07 +0300

    c

---
+++ PlaywrightSpecFlow.NET_SauceDemo/FrameworkConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace PlaywrightSpecFlow.NET_SauceDemo;
+
+public static class FrameworkConstants
+{
+    public static readonly string FilePath = "C:/Users/yury.samsonau/RiderProjects/PlaywrightSpecFlow.NET_SauceDemo/PlaywrightSpecFlow.NET_SauceDemo/Files/NewFile1.txt";
+}
